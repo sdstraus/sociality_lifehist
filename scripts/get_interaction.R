16bc878c2bf6b8ecf89f9b355da4e2e47db72037
Samantha Straus <sstraus@uwsp.edu>
2025-01-07 14:20:11 -0500

    update

--- scripts/get_interaction.R
+++ scripts/get_interaction.R
@@ -1,5 +1,5 @@
 # Date created: 21 July 2023
-# Date updated: 22 July 2023
+# Date updated: 20 March 2024
 # Updated by: Sam Straus
 
 
@@ -7,11 +7,13 @@
 rm(list=ls()) 
 
 # # LIBRARIES # #
-library(tidyverse)
-library("dplyr")
+
 library("tibble")
 library('rglobi')
 library(googlesheets4)
+
+library(tidyverse)
+library("dplyr")
 # 
 # # # FUNCTIONS # #
 # source("scripts/functions.R")
@@ -24,7 +26,7 @@ interaction_types
 
 # applies get_interactions(taxon = "scientific name", interaction.type = c(list))
 # e.g.
-test <- get_interactions(taxon = "Homo sapiens", interaction.type = c("preysOn", "kills"))
+test <- get_interactions(taxon = "Lycalopex culpaeus", interaction.type = c("preysOn", "kills"))
 ## ok, for some reason records are duplicated...
 
 # get the count of the number of unique taxon names, to start
@@ -73,6 +75,49 @@ for(sp in 1:length(sp_list_bees)){
 }
 
 
+
+#### Canids ######
+
+sp_list_canids <- c("Otocyon megalotis", "Vulpes pallida" , "Vulpes zerda", "Cuon alpinus", 
+                    "Canis adustus", "Canis mesomelas", "Canis simensis", "Lycaon pictus", 
+                    "Vulpes rueppelli", "Vulpes chama", "Urocyon littoralis", "Vulpes velox", 
+                    "Canis rufus", "Vulpes macrotis", "Vulpes lagopus", "Canis latrans", 
+                    "Urocyon cinereoargenteus")
+
+# , "Vulpes vulpes", "Canis lupus"
+
+new_df <- data.frame(species = sp_list_canids, num.foods = rep(0, times = length(sp_list_canids)))
+
+for(sp in 1:length(sp_list_canids)){
+  if(sp == 1){  
+    sp_name <- sp_list_canids[sp]
+    temp_df <- get_interactions(taxon = sp_name, interaction.type = c("eats", "preysOn", "kills", "acquiresNutrientsFrom"))
+    new_df$num.foods[sp] <- length(unique(temp_df$target_taxon_name))
+    diet_items_df <- temp_df
+  } else {
+    sp_name <- sp_list_canids[sp]
+    temp_df <- get_interactions(taxon = sp_name, interaction.type = c("eats", "preysOn", "kills", "acquiresNutrientsFrom"))
+    new_df$num.foods[sp] <- length(unique(temp_df$target_taxon_name))
+    diet_items_df <- rbind(diet_items_df, temp_df)
+  }
+}
+
+
+# Split the target_taxon_path column by " | " and extract the last element
+diet_items_df$finest_taxon <- sapply(str_split(diet_items_df$target_taxon_path, " \\| "), function(x) tail(x, n=1))
+
+preyTaxa_perCanid <- diet_items_df %>% 
+  group_by(source_taxon_name, finest_taxon) %>% 
+  summarize(recordsPerTax = n()) %>% 
+  ungroup()
+
+numPreyItems_perCanid <- preyTaxa_perCanid %>% 
+  group_by(source_taxon_name) %>% 
+  summarise(nTax = n()) %>% 
+  ungroup()
+  
+
+#########
 
 library(taxize)
 gnr_resolve(sci = 'Dialictus umbripenne') %>% View()
